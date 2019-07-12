36b8fff856224bc9717d9f38ac4efa883e48c533
Patrick Liddy <pjliddy@gmail.com>
2019-07-12 15:15:40 -0400

    Switched to HashRouter to demo pages on gh-pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import ContentList from './views/ContentList';
 import ContentCreate from './views/ContentCreate';
 import ContentEdit from './views/ContentEdit';
@@ -8,12 +8,12 @@ import Header from './Header';
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <HashRouter basename='/'>
         <Header />
         <Route path="/" exact component={ContentList} />
         <Route path="/content/edit/:id" exact component={ContentEdit} />
         <Route path="/content/new" exact component={ContentCreate} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
