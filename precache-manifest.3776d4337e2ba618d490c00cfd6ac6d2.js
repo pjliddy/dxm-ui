1f4e5121c7ad80f66524739734b8a78ec1b56dfc
pjliddy <pjliddy@gmail.com>
2019-07-14 08:42:40 -0400

    Updates

---
+++ precache-manifest.3776d4337e2ba618d490c00cfd6ac6d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73daa4a175a6cca5b948ede83e66e975",
+    "url": "/dxm-ui/index.html"
+  },
+  {
+    "revision": "4d9ae10e0e59434cee8a",
+    "url": "/dxm-ui/static/js/2.41b5f452.chunk.js"
+  },
+  {
+    "revision": "99e3a42936053976720a",
+    "url": "/dxm-ui/static/js/main.9b248031.chunk.js"
+  },
+  {
+    "revision": "b932b5d17f767a7e4785",
+    "url": "/dxm-ui/static/js/runtime~main.4306d79f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dxm-ui/precache-manifest.be4d14bb3eb409fa760abc2d4c90cf78.js"
+  "/dxm-ui/precache-manifest.3776d4337e2ba618d490c00cfd6ac6d2.js"
 );
 
 self.addEventListener('message', (event) => {
